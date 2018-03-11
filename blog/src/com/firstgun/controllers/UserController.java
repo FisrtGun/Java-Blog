58df8fef22183f8fc69b4be5f5356d125aa797a5
沙漠大雁无敌狼 <34479437+chenjunjialinss@users.noreply.github.com>
2018-03-11 17:29:05 +0800

    其他网页做完

--- blog/src/com/firstgun/controllers/UserController.java
+++ blog/src/com/firstgun/controllers/UserController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("userController")
 public class UserController {
+    //去首页
     @RequestMapping("index")
     public String index(){
         return "index";
+    }
+    //去其他
+    @RequestMapping("other")
+    public String other(){
+        return "other";
     }
 }
