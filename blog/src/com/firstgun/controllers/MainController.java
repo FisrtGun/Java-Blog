b3ee522669c905515b5052f07c6601fa543118d2
dycheng514 <dycheng514@gmail.com>
2018-03-12 15:12:04 +0800

    主页控制器提交

--- blog/src/com/firstgun/controllers/MainController.java
+++ blog/src/com/firstgun/controllers/MainController.java
@@ -5,9 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("userController")
-public class UserController {
+public class MainController {
     @RequestMapping("index")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping("/csto")
+    public String csto(){
+        return "cstohome";
+    }
+
+    @RequestMapping("newstitle")
+    public String newstitle(){
+        return "newstitle";
     }
 }
